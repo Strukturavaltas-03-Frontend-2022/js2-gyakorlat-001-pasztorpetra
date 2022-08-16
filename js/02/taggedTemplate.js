47ff3c2fb863edb9f2bfce6c7f0f221e6d5392da
Petra <pasztor.petra1998@gmail.com>
2022-08-16 12:52:00 +0200

    2.feladat alakul

--- js/02/taggedTemplate.js
+++ js/02/taggedTemplate.js
@@ -2,9 +2,8 @@
 
 const taggedTemplate = (texts, ...values) => 
     texts.map((text, index) => 
-    `${text& !== '' ? `<em>${text}</em>` : ''}
-     ${values[index] ? `<strong>${values[index]}</strong>` : ''}`)
-    .join('');
-
+        `${text && text !== '' ? `<em>${text}</em>` : ''}
+        ${values[index] ? `<strong>${values[index]}</strong>` : ''}`
+        ).join('');
 
 export default taggedTemplate;
