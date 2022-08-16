7e90b1feafd25ad057bd1b9b0bd7412eabfdac33
Petra <pasztor.petra1998@gmail.com>
2022-08-16 16:56:05 +0200

    kész

--- js/03/concatArrays.js
+++ js/03/concatArrays.js
@@ -1,5 +1,12 @@
 
-const concatArrays = (a, ...arrays) => {};
+const concatArrays = (a, ...arrays) => {
+    const flatArray = [...arrays].flat();
+    const array = [...new Set(flatArray)];
+    const cleanArray = array.filter(item => !item.includes(a));
+    const trimmedArray = cleanArray.map(item=> item.trimEnd());
+
+    return trimmedArray;
+};
 
 
 export default concatArrays;
