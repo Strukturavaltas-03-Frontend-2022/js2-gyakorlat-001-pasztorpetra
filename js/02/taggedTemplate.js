7c2f65fb6d6e61ff202e80009d2e9a04d5d1acc3
Petra <pasztor.petra1998@gmail.com>
2022-08-16 12:41:09 +0200

    2.feladat félig kész

---
+++ js/02/taggedTemplate.js
@@ -0,0 +1,10 @@
+
+
+const taggedTemplate = (texts, ...values) => 
+    texts.map((text, index) => 
+    `${text& !== '' ? `<em>${text}</em>` : ''}
+     ${values[index] ? `<strong>${values[index]}</strong>` : ''}`)
+    .join('');
+
+
+export default taggedTemplate;
